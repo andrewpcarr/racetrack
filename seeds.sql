be6ac00478ec2970c4cdfc83bb240b44736f3a81
lofisoul <jason.nolfi@gmail.com>
2017-05-06 11:43:31 -0500

    Merging into Jason

---
+++ seeds.sql
@@ -0,0 +1,30 @@
+use racetrack_db; 
+
+INSERT INTO Race (race_name, city, state, country, venue, distance, race_month, water_type, swim_start,
+bike_course, run_type, women_only) VALUES 
+("Rookie", "Austin", "TX", "USA", "Decker Lake", "Super-Sprint", "May", "Fresh", "Waves: From Shore", "Closed", 
+"Trail", false), 
+("Tri Rock", "Austin", "TX", "USA", "Auditorium Shores", "Sprint", "September", "Fresh", "Time Trial", "Closed", 
+"Road", false), 
+("Na Wahine", "Honolulu", "HI", "USA", "Waikiki", "Sprint", "September", "Ocean", "Waves: Deep Water", "Open", 
+"Road", true), 
+("Jack's Generic", "Pflugerville", "TX", "USA", "Lake Pflugerville", "Sprint", "August", "Fresh", "Waves: From Shore", "Open", 
+"Trail", false), 
+("Marble Falls", "Marble Falls", "TX", "USA", "Lakeside Park", "Super-Sprint", "July", "Fresh", "Waves: Deep Water", "Open", 
+"Road", false), 
+("Spring Lake", "San Marcos", "TX", "USA", "Meadows Center", "Sprint", "September", "Fresh", "Time Trial", "Open", 
+"Trail", false), 
+("Lake Pflugerville", "Pflugerville", "TX", "USA", "Lake Pflugerville", "Sprint", "June", "Fresh", "Waves: From Shore", "Open", 
+"Trail", false), 
+("Kerrville Triathlon Sprint", "Kerrville", "TX", "USA", "Nimitz Lake", "Sprint", "September", "Fresh", "Waves: From Shore", "Open", 
+"Road", false), 
+("LavaMan", "Waikoloa", "HI", "USA", "Anaehoomalu Bay", "Intermediate", "April", "Ocean", "Waves: From Shore", "Open", 
+"Trail", false), 
+("Kerrville Triathlon Half", "Kerrville", "TX", "USA", "Nimitz Lake", "Half", "September", "Fresh", "Waves: From Shore", "Open", 
+"Road", false); 
+
+
+
+
+
+
