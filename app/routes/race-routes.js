8719979d6a0f109a661affb54481290667b20793
tmhammons91 <terri@terrihammons.com>
2017-05-09 12:04:20 -0500

    updated config file to remove db password. minor updates to race-routes

--- app/routes/race-routes.js
+++ app/routes/race-routes.js
@@ -1,5 +1,6 @@
 var db = require("../models");
 
+
 module.exports = function (app) {
   app.get("/api/race", function (req, res) {
     // 1. Add a join to include all 
@@ -8,24 +9,41 @@ module.exports = function (app) {
       console.log(JSON.stringify(dbRace[0]));  // console logging out first race to check that it works
     });
   });
- 
-//  TH on 5/8--CAN'T GET THE FOLLOWING TO WORK.  WHY?????
-/*
-  app.get("api/race/:id?", function (req, res) {
-    console.log("req params: " + req.params.id);
-    db.Race.findOne({
+
+  //  
+
+ /* app.get("/api/race/:name", function (req, res) {
+    console.log("req params: " + req.params.name);
+    db.Race.findAll({
       where: {
-        id: req.params.id
+        race_name: req.params.name, 
+        //city: req.params.city,
+        //distance: req.params.distance,
+       // swim_start: req.params.swimstart     
       },
-      //include: [db.Review]
+      include: [db.Review]
     }).then(function (dbRace) {
-     res.json(dbRace);
-      console.log(dbRace);
+      res.json(dbRace);
+      //console.log(JSON.stringify(dbRace));
       console.log("does this work?");
     });
     //console.log(dbRace); 
-  });
-*/
-}; 
+  });  */
 
- 
+   app.get("/api/race/results", function(req, res) {
+    console.log("bueller?  bueller?" + req.body.name);
+    db.Race.findAll({
+      where: {
+      race_name: req.body.name,
+     race_month: req.body.month,
+     city: req.body.city,
+      distance: req.body.distance, 
+     swim_start: req.body.start
+      } 
+    })
+    .then(function(dbRace) {
+      res.json(dbRace);
+    });
+  });
+
+};
