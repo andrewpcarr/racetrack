bfc29e8d0a5f67f88bcc60cbfab8c64250d41d0b
Benjamin Mastarone <bcmastarone@Benjamins-MacBook-Pro.local>
2017-05-09 20:14:57 -0500

    changes

--- app/public/js/race_landing_page.js
+++ app/public/js/race_landing_page.js
@@ -11,4 +11,7 @@ $(document).ready(function() {
 //The next part should be a function that sends off the variables for a search. 
 //Then we should redirect to the race-results.html
 
+
+function searchTerms(raceInput )
+
 }
