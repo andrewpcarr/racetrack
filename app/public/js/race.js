5fd4d21ddf315973ece25de9f8a6604ada6886c3
lofisoul <jason.nolfi@gmail.com>
2017-05-12 16:29:54 -0500

    Updating race js to remove modal calls b/c using bootstrap

--- app/public/js/race.js
+++ app/public/js/race.js
@@ -1,18 +1,18 @@
 $(function() {
-  $('.review-race').on('click', function() {
-    $('.modal').fadeIn(500);
-  });
-
-  $('.cancel').on('click', function() {
-    $('.modal').fadeOut(500);
-  });
+  // $('.review-race').on('click', function() {
+  //   $('.modal').fadeIn(500);
+  // });
+  //
+  // $('.cancel').on('click', function() {
+  //   $('.modal').fadeOut(500);
+  // });
 
   // $('.btn-warning').on('click', function() {
   //  $('.btn-secondary').append($("<div class='w3-container w3-center w3-animate-bottom review-confirmation'>\
   //         <h1>Review submitted!</h1>\
   //       </div>"));
   // });
-  // 
+  //
 
   $('.btn.btn-warning.submit-review').on('click', function() {
     createNewReview();
@@ -79,4 +79,3 @@ $(function() {
   }
 
 });
- 
